8d5e3d43c43b8b9893769eefc85cde03364e4f84
Leonardo <info.leonardofalchi@gmail.com>
2025-09-04 19:10:25 +0200

    bonus

--- script.js
+++ script.js
@@ -6,7 +6,24 @@ const mioBottone = document.getElementById("myButton");
 console.log("mioBottone");
 
 //aggiungo evento su bottone
-mioBottone.addEventListener('click',
-    function() {
-        miaLampadina.src="img/yellow_lamp.png"
-    });
+/* mioBottone.addEventListener('click',
+    function () {
+        miaLampadina.src = "img/yellow_lamp.png"
+    }); */
+
+
+//BONUS
+
+//gestisco evento su bottone
+mioBottone.addEventListener("click", accesaSpenta);
+
+//creo funzione
+function accesaSpenta() {
+    if (miaLampadina.src.includes("white")) {
+        miaLampadina.src="img/yellow_lamp.png";
+        mioBottone.textContent = "Spegni";
+    } else {
+        miaLampadina.src="img/white_lamp.png";
+        mioBottone.textContent = "Accendi";
+    }
+}
